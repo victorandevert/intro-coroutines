dbdd2fd9cfd7d5342cdbdddb4bf0c11e80e5d5c4
Victor Andevert <victor.andevert@gmail.com>
2023-02-11 16:31:11 +0100

    task 1: combine users by contributions

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -14,5 +14,6 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> = this.groupBy { it.login }.map {
+    User(it.key, it.value.fold(0) { acc, i -> i.contributions + acc })
+}.sortedByDescending { it.contributions }
